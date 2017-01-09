e876db25b05a785cf157df0466ccd4ec90d0f984
RanganathG <rgovardh@microsoft.com>
2017-01-09 20:43:08 +0530

    Complete Demo

---
+++ src/www/js/components/item-form.jsx
@@ -0,0 +1,31 @@
+"use strict";
+
+const
+    React = require("react");
+
+module.exports = React.createClass({
+    getInitialState: function () {
+        return {
+            newItem: ""
+        }
+    },
+    onChange: function (e) {
+        this.setState({
+            newItem: e.target.value
+        });
+    },
+    addItem: function (e) {
+        this.props.addItem(this.state.newItem);
+        this.setState({
+            newItem: ""
+        });
+    },
+    render: function () {
+        return <form>
+            <label htmlFor="new-item">New Items:</label>
+            <input id="new-item" value={this.state.newItem}
+                onChange={this.onChange} />
+            <button type="button" onClick={this.addItem}> Add Item</button>
+        </form>
+    }
+})
--- src/www/js/site.jsx
+++ src/www/js/site.jsx
@@ -1,1 +1,13 @@
 "use strict";
+
+const
+    React = require("react"),
+    ReactDOM = require("react-dom"),
+    ItemList = require("./components/item-list");
+
+let items = [
+    "item 1", "item 2", "item 3"
+];
+
+ReactDOM.render(<ItemList header="Item List" items={items} />,
+    document.querySelector("[class='container']"));
